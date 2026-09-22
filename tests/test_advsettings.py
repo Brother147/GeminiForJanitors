a53948c9f1a33e6c5efb256253f1f40a751ae1fc
Brother147 <105119480+Brother147@users.noreply.github.com>
2026-09-22 14:51:40 +0300

    +

--- tests/test_advsettings.py
+++ tests/test_advsettings.py
@@ -57,8 +57,7 @@ def test_advset_this(
     mocker: MockerFixture, provider: str, advsettings: dict[str, int | float]
 ):
     """Check that the given advanced settings are being applied."""
-
-    provider_module = "gemini" if provider == "google" else provider
+  
     mock_call = mocker.patch("gfjproxy.streaming.http_client.post")
     mock_response = mocker.Mock()
     mock_response.status_code = 200
